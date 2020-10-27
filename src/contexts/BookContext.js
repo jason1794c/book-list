10db4276672acc79790fc944347e751dd877fcc9
jason1794c <69557789+jason1794c@users.noreply.github.com>
2020-10-27 16:10:47 -0400

    first commit

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+export const BookContext = createContext();
+
+export default function BookContextProvider(props) {
+    const [books, setBook] = useState([
+        {title: 'name of the wind', author: 'patrick orthfuss', id: 1},
+        {titel: 'the final empire', author: 'brandon sanderson', id: 2}
+    ]);
+
+    const addBook = (title, author) => {
+        setBook([...books, { title: title, author: author, id: uuid() }])
+    }
+
+    const removeBook = (id) => {
+        setBook(books.filter(book => book.id != id))
+    }
+
+    return (
+        <div>
+            <BookContext.Provider value={{books, addBook, removeBook}}>
+                {props.children}
+            </BookContext.Provider>
+        </div>
+    )
+}
